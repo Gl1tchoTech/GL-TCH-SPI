e64d3d5c09ba3c73ec6b7e842b7966bae2783091
GL1TCH <ShadowVR7913@gmail.com>
2026-06-16 11:54:57 -0400

    Update lyrics.py

--- app/routers/lyrics.py
+++ app/routers/lyrics.py
@@ -1,8 +1,36 @@
-from fastapi import APIRouter
-from app.services.ytmusic_service import get_lyrics
+from fastapi import APIRouter, HTTPException
 
-router = APIRouter(tags=["Lyrics"])
+from app.services.ytmusic_service import ytmusic
 
-@router.get("/lyrics/{browse_id}")
-def lyrics(browse_id: str):
-    return get_lyrics(browse_id)
+router = APIRouter(
+    tags=["Lyrics"]
+)
+
+
+@router.get("/lyrics/{video_id}")
+def lyrics(video_id: str):
+    try:
+        watch_playlist = ytmusic.get_watch_playlist(
+            videoId=video_id
+        )
+
+        lyrics_browse_id = watch_playlist.get("lyrics")
+
+        if not lyrics_browse_id:
+            raise HTTPException(
+                status_code=404,
+                detail="Lyrics not available"
+            )
+
+        return ytmusic.get_lyrics(
+            lyrics_browse_id
+        )
+
+    except HTTPException:
+        raise
+
+    except Exception as e:
+        raise HTTPException(
+            status_code=500,
+            detail=str(e)
+        )
