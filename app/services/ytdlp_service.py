997093b0f45fad128818827a121e90d75857db33
GL1TCH <ShadowVR7913@gmail.com>
2026-06-16 11:43:02 -0400

    Update ytdlp_service.py

--- app/services/ytdlp_service.py
+++ app/services/ytdlp_service.py
@@ -1,1 +1,22 @@
+import yt_dlp
 
+def get_stream(video_id):
+    url = f"https://www.youtube.com/watch?v={video_id}"
+
+    options = {
+        "quiet": True,
+        "format": "bestaudio/best"
+    }
+
+    with yt_dlp.YoutubeDL(options) as ydl:
+        info = ydl.extract_info(
+            url,
+            download=False
+        )
+
+    return {
+        "id": video_id,
+        "title": info.get("title"),
+        "duration": info.get("duration"),
+        "stream_url": info.get("url")
+    }
