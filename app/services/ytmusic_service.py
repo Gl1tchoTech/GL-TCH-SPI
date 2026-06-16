33a6c129064d18ded2ee6474fc3aff0ab58d457b
GL1TCH <ShadowVR7913@gmail.com>
2026-06-16 11:42:39 -0400

    Update ytmusic_service.py

--- app/services/ytmusic_service.py
+++ app/services/ytmusic_service.py
@@ -1,1 +1,18 @@
+from ytmusicapi import YTMusic
 
+ytmusic = YTMusic()
+
+def search(query):
+    return ytmusic.search(query)
+
+def get_artist(browse_id):
+    return ytmusic.get_artist(browse_id)
+
+def get_album(browse_id):
+    return ytmusic.get_album(browse_id)
+
+def get_playlist(playlist_id):
+    return ytmusic.get_playlist(playlist_id)
+
+def get_lyrics(browse_id):
+    return ytmusic.get_lyrics(browse_id)
