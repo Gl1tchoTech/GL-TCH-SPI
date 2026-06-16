22a39a9267af5e787017b828047f2753cefdbb33
GL1TCH <ShadowVR7913@gmail.com>
2026-06-16 11:42:11 -0400

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,1 +1,26 @@
+from fastapi import FastAPI
 
+from app.routers import (
+    search,
+    albums,
+    artists,
+    playlists,
+    lyrics,
+    stream
+)
+
+app = FastAPI(
+    title="YouTube Music API",
+    version="1.0.0"
+)
+
+app.include_router(search.router)
+app.include_router(albums.router)
+app.include_router(artists.router)
+app.include_router(playlists.router)
+app.include_router(lyrics.router)
+app.include_router(stream.router)
+
+@app.get("/")
+def root():
+    return {"status": "online"}
